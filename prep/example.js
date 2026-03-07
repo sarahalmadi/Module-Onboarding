e91f9d1a1084463f71ad99c29d8d56e875ae4974
Sarah Almadi <sarah666lilith@gmail.com>
2026-03-07 21:26:33 +0100

    Add example.js file with age calculation logic

---
+++ prep/example.js
@@ -0,0 +1,9 @@
+const yearOfBirth = 1997; // declaration
+let currentYear = 2026; // declaration
+
+currentYear++; // statement
+`I am ${currentYear - yearOfBirth} years old`; // statement
+console.log(`I am ${currentYear - yearOfBirth} years old`);
+// Output: I am 29 years old
+console.log("hello there!");
+// Output: hello there!
